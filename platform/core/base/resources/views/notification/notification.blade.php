bcba2ec7b78ced25fcc106256484cfa4e32b822c
helodeepakji <helodeepakji@gmail.com>
2025-01-13 08:50:06 +0530

    js file page

--- platform/core/base/resources/views/notification/notification.blade.php
+++ platform/core/base/resources/views/notification/notification.blade.php
@@ -1,21 +1,18 @@
 @once
-    <div
-        class="offcanvas offcanvas-end"
-        tabindex="-1"
-        id="notification-sidebar"
-        aria-labelledby="notification-sidebar-label"
-        data-url="{{ route('notifications.index') }}"
-        data-count-url="{{ route('notifications.count-unread') }}"
-    >
-        <button
-            type="button"
-            class="btn-close text-reset"
-            data-bs-dismiss="offcanvas"
-            aria-label="Close"
-        ></button>
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="notification-sidebar" aria-labelledby="notification-sidebar-label"
+        data-url="{{ route('notifications.index') }}" data-count-url="{{ route('notifications.count-unread') }}">
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
 
         <div class="notification-content"></div>
     </div>
 
     <script src="{{ asset('vendor/core/core/base/js/notification.js') }}"></script>
+    <script>
+        $(document).ready(function() {
+            $('#skills-select').select2({
+                placeholder: "Select skills",
+                allowClear: true
+            });
+        });
+    </script>
 @endonce
